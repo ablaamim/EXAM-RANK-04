91cf8c5a8d322745628b6d0d4fc699de50ba989a
ablaamim <aku-no-hana@outlook.fr>
2022-12-14 20:55:38 +0100

    fix

--- main.c
+++ main.c
@@ -1,214 +1,229 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ablaamim <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/12/14 20:09:21 by ablaamim          #+#    #+#             */
+/*   Updated: 2022/12/14 20:54:38 by ablaamim         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "microshell.h"
 
-int fork1()
+int	fork1()
 {
-    int pid = fork();
-    if (pid == -1)
-       write(2, "error : fatal\n", sizeof("error : fatal\n"));
-    return pid;
+	int pid = fork();
+
+	if (pid == -1)
+		write(2, "error : fatal\n", sizeof("error : fatal\n"));
+	return pid;
 }
 
-int pipe1(int fd[2])
+int	pipe1(int fd[2])
 {
-    int ret = pipe(fd);
-    if (ret == -1)
-        write(2, "error : fatal\n", sizeof("error : fatal\n"));
-    return ret;
+	int ret = pipe(fd);
+	if (ret == -1)
+		write(2, "error : fatal\n", sizeof("error : fatal\n"));
+	return ret;
 }
 
-void cd(char **cmd)
+void	cd(char **cmd)
 {
-    int ac = 0;
-    while (cmd[ac])
-        ac++;
-    if (ac != 2) {
-        write(2, "error: cd: bad arguments\n", sizeof("error: cd: bad arguments"));
-        return ;
-    }
-    int ret = chdir(cmd[1]);
-    if (ret == -1) {
-        write(2, "error: cd: cannot change directory to ", sizeof("error: cd: cannot change directory to"));
-        putstr(2, cmd[1]);
-        write(2, "\n", 1);
-        return ;
-    }
-    return ;
+	int ac = 0;
+
+	while (cmd[ac])
+		ac++;
+	if (ac != 2)
+	{
+		write(2, "error: cd: bad arguments\n", sizeof("error: cd: bad arguments"));
+		return ;
+	}
+	int ret = chdir(cmd[1]);
+	
+	if (ret == -1)
+	{
+		write(2, "error: cd: cannot change directory to ", sizeof("error: cd: cannot change directory to"));
+		putstr(2, cmd[1]);
+		write(2, "\n", 1);
+		return ;
+	}
+	return ;
 }
 
-void exec_ast(t_tree *tree, char **env)
+void	exec_ast(t_tree *tree, char **env)
 {
-    if (!tree)
-        return ;
-    
-    if (tree->type == EXEC) {
-        
-        if (strcmp(tree->cmd[0], "cd") == 0)
-        {
-            return cd(tree->cmd);
-        }
+	if (!tree)
+		return ;
 
-        int pid = fork1();
-        if (pid == 0) {
-           execve(tree->cmd[0], tree->cmd, env);
-           write(2, "error: cannot execute ", sizeof("error: cannot execute"));
-           putstr(2, tree->cmd[0]);
-           write(2, "\n", 1);
-           exit(1);
-        }
-        waitpid(pid, NULL ,0);
-        return ;
-    }
-
-    if (tree->type == SEMICOLON)
-    {
-        exec_ast(tree->left, env);
-        exec_ast(tree->right, env);
-        return ;
-    }
-
-    if (tree->type == PIPE)
-    {
-        int pfd[2];
-
-        pipe1(pfd);
-        if (fork1() == 0)
-        {
-            close(1);
-            dup2(pfd[1], 1);
-            close(pfd[0]);
-            close(pfd[0]);
-            exec_ast(tree->left, env);
-            exit(0);
-        }
-        if (fork1() == 0)
-        {
-            close(0);
-            dup2(pfd[0], 0);
-            close(pfd[0]);
-            close(pfd[1]);
-            exec_ast(tree->right, env);
-            exit(0);
-        }
-        close(pfd[0]);
-        close(pfd[1]);
-        waitpid(-1, 0, 0);
-        waitpid(-1, 0, 0);
-        return ;
-    }
-    return ;
+	if (tree->type == EXEC)
+	{
+		if (strcmp(tree->cmd[0], "cd") == 0)
+		{
+			return cd(tree->cmd);
+		}
+		int pid = fork1();
+		if (pid == 0)
+		{
+			execve(tree->cmd[0], tree->cmd, env);
+			write(2, "error: cannot execute ", sizeof("error: cannot execute"));
+			putstr(2, tree->cmd[0]);
+			write(2, "\n", 1);
+			exit(1);
+		}
+		waitpid(pid, NULL ,0);
+		return ;
+	}
+	if (tree->type == SEMICOLON)
+	{
+		exec_ast(tree->left, env);
+		exec_ast(tree->right, env);
+		return ;
+	}
+	if (tree->type == PIPE)
+	{
+		int pfd[2];
+		pipe1(pfd);
+		if (fork1() == 0)
+		{
+			close(1);
+			dup2(pfd[1], 1);
+			close(pfd[0]);
+			close(pfd[0]);
+			exec_ast(tree->left, env);
+			exit(0);
+		}
+		if (fork1() == 0)
+		{
+			close(0);
+			dup2(pfd[0], 0);
+			close(pfd[0]);
+			close(pfd[1]);
+			exec_ast(tree->right, env);
+			exit(0);
+		}
+		close(pfd[0]);
+		close(pfd[1]);
+		waitpid(-1, 0, 0);
+		waitpid(-1, 0, 0);
+		return ;
+	}
+	return ;
 }
 
-int semico_check(char ***av)
+int	semico_check(char ***av)
 {
-    int flag = 0;
-    while (**av && strcmp(**av, ";") == 0)
-    {
-        flag = 1;
-        **av = NULL;
-        (*av)++;
-    }
-    return flag;
+	int flag
+	flag = 0;
+	while (**av && strcmp(**av, ";") == 0)
+	{
+		flag = 1;
+		**av = NULL;
+		(*av)++;
+	}
+	return flag;
 }
 
-t_tree *parse_list(char ***av)
+t_tree	*parse_list(char ***av)
 {
-    t_tree *ret;
+	t_tree	*ret;
 
-    if (!**av) return NULL;
-    ret = tree_constructor(EXEC, *av, 0, 0);
-    while (**av && !strcmp(**av, ";") && !strcmp(**av, "|"))
-    {
-        (*av)++;
-    }
-    return (ret);
+	if (!**av)
+		return NULL;
+	ret = tree_constructor(EXEC, *av, 0, 0);
+	while (**av && !strcmp(**av, ";") && !strcmp(**av, "|"))
+		(*av)++;
+	return (ret);
 }
 
 t_tree *parse_pline(char ***av)
 {
-    t_tree *ret;
+	t_tree	*ret;
 
-    if (!**av)
-        return NULL;
-    ret = parse_list(av);
-    if (!ret)
-        return NULL;
-    while (**av && strcmp(**av, "|") == 0)
-    {
-        **av = NULL;
-        (*av)++;
-        ret = tree_constructor(PIPE, 0, ret, NULL);
-        ret->right = parse_list(av);
-        if (!ret->right)
-            return NULL;
-    }
-    return ret;
+	if (!**av)
+		return NULL;
+	ret = parse_list(av);
+	if (!ret)
+		return NULL;
+	while (**av && strcmp(**av, "|") == 0)
+	{
+		**av = NULL;
+		(*av)++;
+		ret = tree_constructor(PIPE, 0, ret, NULL);
+		ret->right = parse_list(av);
+		if (!ret->right)
+			return NULL;
+	}
+	return ret;
 }
 
-t_tree *parse_cmd(char ***av)
+t_tree	*parse_cmd(char ***av)
 {
-    t_tree *ret;
+	t_tree *ret;
 
-    if (!**av)
-        return NULL;
-    ret = parse_pline(av);
-    if (!ret)
-        return NULL;
-    if (**av && semico_check(av)) {
-        ret = tree_constructor(SEMICOLON, 0, ret, parse_cmd(av));
-    }
-    return ret;
+	if (!**av)
+		return NULL;
+	ret = parse_pline(av);
+	if (!ret)
+		return NULL;
+	if (**av && semico_check(av))
+	{
+		ret = tree_constructor(SEMICOLON, 0, ret, parse_cmd(av));
+	}
+	return ret;
 }
 
 t_tree *tree_constructor(t_type type, char **cmd, t_tree *left, t_tree *right)
 {
-    t_tree *ret = malloc(sizeof(t_tree));
-    if (!ret)
-        write(2, "error : fatal\n", 14);
-    ret->type = type;
-    ret->cmd = cmd;
-    ret->right = right;
-    ret->left = left;
-    return ret;
+	t_tree *ret = malloc(sizeof(t_tree));
+	if (!ret)
+		write(2, "error : fatal\n", 14);
+	ret->type = type;
+	ret->cmd = cmd;
+	ret->right = right;
+	ret->left = left;
+	return ret;
 }
 
 void tree_debug(t_tree *tree, int level)
 {
-    if (!tree)
-        return ;
-    for (int i = 0; i < level; i++)
-        printf("\t\t");
-    if (tree->type == PIPE)
-        printf("PIPE\n");
-    else if (tree->type == SEMICOLON)
-        printf("FG\n");
-    else if (tree->type == EXEC)
-    {
-        printf("EXEC: ");
-        for (int i = 0; tree->cmd[i]; i++)
-            printf(" %s  ", tree->cmd[i]);
-        printf("\n");
-    }
-    else
-        printf("UNKNOWN CMD\n");
-    tree_debug(tree->left, level + 1);
-    tree_debug(tree->right, level + 1);
+	if (!tree)
+		return ;
+	for (int i = 0; i < level; i++)
+		printf("\t\t");
+	if (tree->type == PIPE)
+		printf("PIPE\n");
+	else if (tree->type == SEMICOLON)
+		printf("FG\n");
+	else if (tree->type == EXEC)
+	{
+		printf("EXEC: ");
+		for (int i = 0; tree->cmd[i]; i++)
+			printf(" %s  ", tree->cmd[i]);
+		printf("\n");
+	}
+	else
+		printf("UNKNOWN CMD\n");
+	tree_debug(tree->left, level + 1);
+	tree_debug(tree->right, level + 1);
 }
 
 int main(int ac, char **av, char **env)
 {
-    av++;
+	av++;
 
-    t_tree *tree = parse_cmd(&av);
-    tree_debug(tree, 0);
-    exec_ast(tree, env);
-    exit(0);
-    (void)ac;
-    (void)av;
-    (void)env;
+	t_tree *tree = parse_cmd(&av);
+	tree_debug(tree, 0);
+	exec_ast(tree, env);
+	exit(0);
+	(void)ac;
+	(void)av;
+	(void)env;
 }
 
 void putstr(int fd, char *str)
 {
-    while (*str)
-        write(fd, str++, 1);
+	while (*str)
+		write(fd, str++, 1);
 }
--- microshell.h
+++ microshell.h
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   microshell.h                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ablaamim <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/12/14 20:54:52 by ablaamim          #+#    #+#             */
+/*   Updated: 2022/12/14 20:55:24 by ablaamim         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef MICROSHELL_H
 # define MICROSHELL_H
 
@@ -8,8 +20,7 @@
 #include <signal.h>
 #include <sys/wait.h>
 
-
-typedef enum	e_type
+typedef enum e_type
 {
 	PIPE = 1,
 	SEMICOLON,
@@ -21,13 +32,13 @@ typedef struct	s_tree
 	t_type			type;
 	char			**cmd;
 	struct s_tree	*left;
-    struct s_tree	*right;
+	struct s_tree	*right;
 }	t_tree;
 
-void tree_debug(t_tree *tree, int level);
-int semico_check(char ***av);
-int fork1();
-int pipe1(int fd[2]);
+void	tree_debug(t_tree *tree, int level);
+int		semico_check(char ***av);
+int		fork1();
+int		pipe1(int fd[2]);
 t_tree	*parse_cmd(char ***av);
 void	exec_ast(t_tree *tree, char **env);
 void	putstr(int fd, char *str);
